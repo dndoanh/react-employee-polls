af00b3757928641d1658e1164a11595c0e883ff0
Doanh Duong <dndoanh@gmail.com>
2023-08-27 09:27:02 +0800

    Add file

---
+++ src/pages/LeaderboardPage.js
@@ -0,0 +1,44 @@
+import { Container, Figure, Table } from "react-bootstrap";
+import { connect } from "react-redux";
+
+const LeaderBoardPage = ({ users }) => {
+  return (
+    <Container>
+      <h1 className="text-center">Leaderboard</h1>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>Users</th>
+          <th>Answered</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((usr, idx) => {
+          const { name, avatarURL, answers, questions, id } = users[idx];
+          return (
+            <tr key={idx}>
+              <td>
+                <Figure>
+                  {avatarURL && (
+                    <Figure.Image width={80} height={80} src={avatarURL} />
+                  )}
+                  <Figure.Caption>{`${name} (${id})`}</Figure.Caption>
+                </Figure>
+              </td>
+              <td>{Object.keys(answers).length}</td>
+              <td>{Object.keys(questions).length}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+    </Container>
+  );
+};
+
+const mapStateToProps = ({ users }) => ({
+  users: Object.keys(users).map((key) => users[key]),
+});
+
+export default connect(mapStateToProps)(LeaderBoardPage);
